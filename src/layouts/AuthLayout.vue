6beb290644341cd5bf5ec5535a06ad76de02b953
SangNef <sang.tv2614@gmail.com>
2025-04-16 10:04:41 +0700

    thêm footer login

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,14 +1,18 @@
 <template>
-  <div>
+  <div class="w-full h-screen overflow-hidden relative">
     <Header />
     <div class="mt-7 px-5">
       <slot />
+    </div>
+    <div class="absolute bottom-0 left-0 p-4 w-full">
+      <img :src="footer" alt="" class="max-h-13 max-w-[90%] mx-auto">
     </div>
   </div>
 </template>
 
 <script setup>
 import Header from '@/components/Auth/Header.vue';
+import footer from '@/assets/images/footer.png'
 
 </script>
 
