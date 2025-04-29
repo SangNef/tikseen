43a742a84de1e471bc428869e1e03abb97aa9d5e
VIPPRO123 <VIPPRO123@gmail.com>
2025-04-30 04:08:47 +0700

    Tailwind 3

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [tailwindcss(), vue()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "~": "/src",
       "@client": "/src",
     },
-  }
-}) 
+  },
+});
