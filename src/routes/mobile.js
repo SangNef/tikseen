cea71a20c3361166a37ab4205eee4dc1cf351ec2
SangNef <sang.tv2614@gmail.com>
2025-06-16 22:29:41 +0700

    them man dang ky va sua các svg show password

--- src/routes/mobile.js
+++ src/routes/mobile.js
@@ -1,201 +1,220 @@
 // src/routes/mobile.js
-import ForgotPassword from '@/views/mobile/Auth/ForgotPassword.vue';
-import { Login } from '@/views/mobile/Auth';
-import { Earn, Envelope, Event, EventDetail, Exchange, Home, Invite, Partner, Vip } from '@/views/mobile/Home';
-import { Explore } from '@/views/mobile/Explore';
-import { Deposit, Future, MainWallet, Transaction, Wallet, Withdraw } from '@/views/mobile/Wallet';
-import { Account, Setting } from '@/views/mobile/Account';
-import InviteReward from '@/views/mobile/Home/InviteReward.vue';
-import { Trade } from '@/views/mobile/Trade';
-import { FutureScreen } from '@/views/mobile/Futute';
+import ForgotPassword from "@/views/mobile/Auth/ForgotPassword.vue";
+import { Login, Register } from "@/views/mobile/Auth";
+import {
+    Earn,
+    Envelope,
+    Event,
+    EventDetail,
+    Exchange,
+    Home,
+    Invite,
+    InviteReward,
+    Partner,
+    Vip,
+} from "@/views/mobile/Home";
+import { Explore } from "@/views/mobile/Explore";
+import { Deposit, Future, MainWallet, Transaction, Wallet, Withdraw } from "@/views/mobile/Wallet";
+import { Account, Setting } from "@/views/mobile/Account";
+import { Trade } from "@/views/mobile/Trade";
+import { FutureScreen } from "@/views/mobile/Futute";
 
 const MobileRoutes = [
     {
-        path: '/login',
-        name: 'Login',
+        path: "/login",
+        name: "Login",
         component: Login,
         meta: {
-            layout: 'AuthLayout',
+            layout: "AuthLayout",
             requiresAuth: false,
         },
     },
     {
-        path: '/forgot-password',
-        name: 'ForgotPassword',
+        path: "/register",
+        name: "Register",
+        component: Register,
+        meta: {
+            layout: "AuthLayout",
+            requiresAuth: false,
+        },
+    },
+    {
+        path: "/forgot-password",
+        name: "ForgotPassword",
         component: ForgotPassword,
         meta: {
-            layout: 'AuthLayout',
+            layout: "AuthLayout",
             guest: false,
         },
     },
     {
-        path: '/',
-        name: 'Home',
+        path: "/",
+        name: "Home",
         component: Home,
         meta: {
-            layout: 'MainLayout',
+            layout: "MainLayout",
             guest: true,
         },
     },
     {
-        path: '/explore',
-        name: 'Explore',
+        path: "/explore",
+        name: "Explore",
         component: Explore,
         meta: {
-            layout: 'MainLayout',
+            layout: "MainLayout",
             requiresAuth: true,
         },
     },
     {
-        path: '/wallet',
-        name: 'Wallet',
+        path: "/wallet",
+        name: "Wallet",
         component: Wallet,
         meta: {
-            layout: 'MainLayout',
+            layout: "MainLayout",
             requiresAuth: true,
         },
     },
     {
-        path: '/trade',
-        name: 'Trade',
+        path: "/trade",
+        name: "Trade",
         component: Trade,
         meta: {
-            layout: 'MainLayout',
+            layout: "MainLayout",
             requiresAuth: true,
         },
     },
     {
-        path: '/futures',
-        name: 'FutureScreen',
+        path: "/futures",
+        name: "FutureScreen",
         component: FutureScreen,
         meta: {
-            layout: 'MainLayout',
+            layout: "MainLayout",
             requiresAuth: true,
         },
     },
     {
-        path: '/account-settings/center',
-        name: 'Account',
+        path: "/account-settings/center",
+        name: "Account",
         component: Account,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/account-settings/acc-info',
-        name: 'Setting',
+        path: "/account-settings/acc-info",
+        name: "Setting",
         component: Setting,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/withdrawal',
-        name: 'Withdraw',
+        path: "/withdrawal",
+        name: "Withdraw",
         component: Withdraw,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/deposit',
-        name: 'Deposit',
+        path: "/deposit",
+        name: "Deposit",
         component: Deposit,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/tx-history',
-        name: 'Transaction',
+        path: "/tx-history",
+        name: "Transaction",
         component: Transaction,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/main-wallet',
-        name: 'MainWallet',
+        path: "/main-wallet",
+        name: "MainWallet",
         component: MainWallet,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/future-wallet',
-        name: 'FutureWallet',
+        path: "/future-wallet",
+        name: "FutureWallet",
         component: Future,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/events',
-        name: 'Events',
+        path: "/events",
+        name: "Events",
         component: Event,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/events/:id',
-        name: 'EventDetail',
+        path: "/events/:id",
+        name: "EventDetail",
         component: EventDetail,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/red-envelope',
-        name: 'Envelop',
+        path: "/red-envelope",
+        name: "Envelop",
         component: Envelope,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/exchange',
-        name: 'Exchange',
+        path: "/exchange",
+        name: "Exchange",
         component: Exchange,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/vip',
-        name: 'Vip',
+        path: "/vip",
+        name: "Vip",
         component: Vip,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/invite',
-        name: 'Invite',
+        path: "/invite",
+        name: "Invite",
         component: Invite,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/invite-reward',
-        name: 'InviteReward',
+        path: "/invite-reward",
+        name: "InviteReward",
         component: InviteReward,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/partner',
-        name: 'Partner',
+        path: "/partner",
+        name: "Partner",
         component: Partner,
         meta: {
             requiresAuth: true,
         },
     },
     {
-        path: '/overnight-interest',
-        name: 'Earn',
+        path: "/overnight-interest",
+        name: "Earn",
         component: Earn,
         meta: {
             requiresAuth: true,
--- src/views/mobile/Auth/index.js
+++ src/views/mobile/Auth/index.js
@@ -1,2 +1,3 @@
-export { default as Login } from './Login.vue';
-export { default as ForgotPassword } from './ForgotPassword.vue';
+export { default as Login } from "./Login.vue";
+export { default as ForgotPassword } from "./ForgotPassword.vue";
+export { default as Register } from "./Register.vue";
